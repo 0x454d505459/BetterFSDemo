5bced46ed90ee38698708dffee33817094df7ab3
0x454d505459 <40660493+0x454d505459@users.noreply.github.com>
2022-03-16 16:58:07 +0100

    Rewrite, added cheat patches

--- Plugin.cs
+++ Plugin.cs
@@ -3,31 +3,76 @@ using BepInEx.Configuration;
 using HarmonyLib;
 using FractalSpace;
 using UnityEngine;
+using System.Collections;
+using UniverseLib;
+using UniverseLib.Input;
+using System;
+using System.Text;
+using System.Linq;
 
 namespace BetterFSDemo
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-		private ConfigEntry<bool> testcfg;
-        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
-    
-        private void Awake()
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);     
+        public void Awake()
         {
             // Plugin startup logic
             harmony.PatchAll();
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-            testcfg = Config.Bind("General", "Lot of ammos", true, "Makes you start with 999 ammos");
 
         }
+        static bool cheats = false;
 
-        // [HarmonyPatch(typeof(GunController), "RefreshTaserModules")]
-        // class gun_fixes {
-        //     [HarmonyPostfix]
-        //     static void get_modules(ref GameObject scopeModule) {
-        //         scopeModule.SetActive(value: true);
-        //     }
+        public static void Update()
+        {
+            if(InputManager.GetKeyDown(KeyCode.K))
+            {
+                cheats = !cheats;
+            }
+        }
 
-        // }
+        [HarmonyPatch(typeof(Controls), nameof(Controls.ToggleDevMode))]
+        class Patch
+        {
+            static AccessTools.FieldRef<Controls, bool> debugRef = AccessTools.FieldRefAccess<Controls, bool>("debug");
+            static bool Prefix(Controls __instance)
+            {
+
+                debugRef(__instance) = !debugRef(__instance);
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(Controls), nameof(Controls.DamageCharacter), new Type[] {typeof(int)})]
+        class NoDamage
+        {
+            static bool Prefix()
+            {
+                if(cheats)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Controls), nameof(Controls.CanKillPlayer))]
+        class NoKill
+        {
+            static bool Prefix(ref bool __result)
+            {
+                if(cheats)
+                {
+                    __result = false;
+                    return false;
+                }
+                
+                return true;
+            }
+        }
+
+               
     }
 }
