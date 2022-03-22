d99a9cdc7e3810ad5681f3ec002b0b4f87b854cb
0x454d505459 <40660493+0x454d505459@users.noreply.github.com>
2022-03-22 16:55:29 +0100

    Added more patches

--- Plugin.cs
+++ Plugin.cs
@@ -15,62 +15,15 @@ namespace BetterFSDemo
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);     
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        private ConfigEntry<bool> testcfg;  
         public void Awake()
         {
             // Plugin startup logic
             harmony.PatchAll();
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            testcfg = Config.Bind("General", "Lot of ammos", true, "Makes you start with 999 ammos");
 
-        }
-        static bool cheats = false;
-
-        public static void Update()
-        {
-            if(InputManager.GetKeyDown(KeyCode.K))
-            {
-                cheats = !cheats;
-            }
-        }
-
-        [HarmonyPatch(typeof(Controls), nameof(Controls.ToggleDevMode))]
-        class Patch
-        {
-            static AccessTools.FieldRef<Controls, bool> debugRef = AccessTools.FieldRefAccess<Controls, bool>("debug");
-            static bool Prefix(Controls __instance)
-            {
-
-                debugRef(__instance) = !debugRef(__instance);
-                return false;
-            }
-        }
-
-        [HarmonyPatch(typeof(Controls), nameof(Controls.DamageCharacter), new Type[] {typeof(int)})]
-        class NoDamage
-        {
-            static bool Prefix()
-            {
-                if(cheats)
-                {
-                    return false;
-                }
-                return true;
-            }
-        }
-
-        [HarmonyPatch(typeof(Controls), nameof(Controls.CanKillPlayer))]
-        class NoKill
-        {
-            static bool Prefix(ref bool __result)
-            {
-                if(cheats)
-                {
-                    __result = false;
-                    return false;
-                }
-                
-                return true;
-            }
         }
 
                
